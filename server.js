56461f5c198067734f88ed0fea5d0f7d0f9150b6
Cyril lopes <82991076+cyrillopes@users.noreply.github.com>
2024-05-25 16:55:00 -0400

    Update server.js
    Updated refresh token in the server js file

--- server.js
+++ server.js
@@ -28,8 +28,57 @@ app.get('/', async (req, res, next) => {
   }
 });
 
-app.get('/auth/zoom', (req, res, next) => {
+app.get('/auth/zoom', async (req, res, next) => {
   const clientId = process.env.ZOOM_API_KEY;
+  const redirect_uri = encodeURIComponent(process.env.REDIRECT_URI);
+  const responseType = 'code';
+  const authorizationUrl = `https://zoom.us/oauth/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirect_uri}`;
+
+  res.redirect(authorizationUrl);
+});
+
+app.get('/callback', async (req, res, next) => {
+  const code = req.query.code;
+  if (!code) return res.status(400).send('No code provided');
+  try {
+    const response = await axios.post('https://zoom.us/oauth/token', null, {
+      params: {
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: process.env.REDIRECT_URI,
+      },
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.ZOOM_API_KEY}:${process.env.ZOOM_API_SECRET}`
+        ).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    console.log(response);
+    res.json(response.data);
+  } catch (error) {
+    console.log('error', error);
+    res.send('Error obtaining token');
+  }
+});
+
+app.get('/refreshToken', async (req, res, next) => {
+  try {
+    const refresh_token = req.query.refreshToken;
+    const response = await axios.post('https://zoom.us/oauth/token', null, {
+      params: {
+        grant_type: 'refresh_token',
+        refresh_token,
+      },
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.ZOOM_API_KEY}:${process.env.ZOOM_API_SECRET}`
+        ).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    res.json(response.data);
+  } catch (error) {}
 });
 
 app.listen(port, () => {
